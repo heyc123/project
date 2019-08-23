d61abf957b8b10bf0252a380963fbb25184fbe47
heyc <15958145241m@sina.cn>
2019-08-23 22:10:00 +0800

    Create RequestXml2.java

---
+++ sendinfo-api/src/main/java/com/example/heyc/dto/RequestXml2.java
@@ -0,0 +1,23 @@
+package com.example.heyc.dto;
+
+import javax.xml.bind.annotation.XmlRootElement;
+
+import lombok.Data;
+
+
+//--???/测试更新
+
+//--1测试更新
+
+
+@Data
+@XmlRootElement
+public class RequestXml2 {
+
+	private String name;
+	
+	private String age;
+	
+	private String sign;
+	
+}
